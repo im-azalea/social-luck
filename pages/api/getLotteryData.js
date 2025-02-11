a742b006c8e0d2c638930a35b9fe13c2de1f16ac
Azalea <100938732+im-azalea@users.noreply.github.com>
2025-02-11 09:02:33 +0700

    Update getLotteryData.js

--- pages/api/getLotteryData.js
+++ pages/api/getLotteryData.js
@@ -6,7 +6,6 @@ export default async function handler(req, res) {
   const filePath = "lottery.json";
   const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
   
-  // Default lottery data
   let lotteryData = {
     currentRound: 1,
     participants: [],
@@ -23,22 +22,9 @@ export default async function handler(req, res) {
     });
     const content = Buffer.from(data.content, 'base64').toString();
     lotteryData = JSON.parse(content);
+    console.log("lotteryData fetched:", lotteryData);
   } catch (error) {
-    console.log("lottery.json tidak ditemukan. Inisialisasi data baru.");
-    // Jika file belum ada, buat file baru dengan data default
-    try {
-      const initialData = lotteryData;
-      const updatedContent = Buffer.from(JSON.stringify(initialData, null, 2)).toString('base64');
-      await octokit.repos.createOrUpdateFileContents({
-        owner,
-        repo,
-        path: filePath,
-        message: "Initialize lottery data",
-        content: updatedContent,
-      });
-    } catch (err) {
-      console.error("Error initializing lottery data:", err);
-    }
+    console.error("Error fetching lotteryData:", error);
   }
   
   res.status(200).json(lotteryData);
